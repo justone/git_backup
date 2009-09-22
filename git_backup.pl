4fd264c8769e61421d3455efeb602faaf1efa98b
Nate Jones <nate@endot.org>
2009-09-21 18:14:15 -0700

    perltidy'd

--- git_backup.pl
+++ git_backup.pl
@@ -11,18 +11,12 @@ Getopt::Long::Configure("no_auto_abbrev");
 
 my %options;
 my $opts_ok = GetOptions(
-    \%options,
-    'help|?|h',
-    'man',
-    'path|p=s',
-    'remote|r=s',
-    'database|d=s',
-    'database-dir|f=s',
-    'prefix|o=s',
-    'commit-message|c=s',
-    'test|t',
-    'mysql-defaults|x=s',
-    'verbose|v',
+    \%options,            'help|?|h',
+    'man',                'path|p=s',
+    'remote|r=s',         'database|d=s',
+    'database-dir|f=s',   'prefix|o=s',
+    'commit-message|c=s', 'test|t',
+    'mysql-defaults|x=s', 'verbose|v',
 );
 
 pod2usage(2) if !$opts_ok;
@@ -34,113 +28,136 @@ my %conf = %options;
 
 # we at least need path
 pod2usage(2) if !$options{'path'};
+
 # default remote for git
 $conf{'remote'} ||= 'backup';
+
 # default commit message for git
 $conf{'commit-message'} ||= 'updated';
+
 # default database dir is nothing
 $conf{'database-dir'} ||= '';
 
 my $defaults_file_option = "";
-if ($conf{'mysql-defaults'}) {
+if ( $conf{'mysql-defaults'} ) {
     $defaults_file_option = " --defaults-file=$conf{'mysql-defaults'} ";
 }
 
 print_configuration() if $conf{'verbose'};
 
 print "Changing to: $options{'path'}\n";
-chdir $options{'path'} || die "unable to change to directory $options{'path'}";
+chdir $options{'path'}
+    || die "unable to change to directory $options{'path'}";
 
 # first, if a database is specified, dump its tables
-if ($conf{'database'}) {
+if ( $conf{'database'} ) {
     print "Database specified, dumping tables.\n";
 
-    if ($conf{'database-dir'} ) {
-        if (! (-e $conf{'database-dir'})) {
-            run_command("/bin/mkdir -p $conf{'database-dir'}", {modifies => 1});
+    if ( $conf{'database-dir'} ) {
+        if ( !( -e $conf{'database-dir'} ) ) {
+            run_command( "/bin/mkdir -p $conf{'database-dir'}",
+                { modifies => 1 } );
         }
         $conf{'database-dir'} .= "/" unless $conf{'database-dir'} =~ /\/$/;
     }
 
-    my @table_list = split(/\n/, run_command("/usr/bin/mysql $defaults_file_option --silent $conf{'database'} -e \"show tables\""));
-    if ($conf{'prefix'}) {
+    my @table_list = split(
+        /\n/,
+        run_command(
+            "/usr/bin/mysql $defaults_file_option --silent $conf{'database'} -e \"show tables\""
+        )
+    );
+    if ( $conf{'prefix'} ) {
         @table_list = grep {/^$conf{'prefix'}/} @table_list;
     }
-    print "Tables to dump: ".join(",", @table_list).".\n" if $conf{'verbose'};
+    print "Tables to dump: " . join( ",", @table_list ) . ".\n"
+        if $conf{'verbose'};
     foreach my $table (@table_list) {
         print "Dumping $table.\n";
-        run_command("/usr/bin/mysqldump $defaults_file_option --extended-insert=FALSE $conf{'database'} $table | /bin/sed 's/ AUTO_INCREMENT=[0-9]\\+//' > $conf{'database-dir'}$table.dump.sql", {modifies => 1});
+        run_command(
+            "/usr/bin/mysqldump $defaults_file_option --extended-insert=FALSE $conf{'database'} $table | /bin/sed 's/ AUTO_INCREMENT=[0-9]\\+//' > $conf{'database-dir'}$table.dump.sql",
+            { modifies => 1 }
+        );
     }
 }
 
 # now, make sure everything is checked in and then push it to the backup remote
 print "Checking git status.\n";
-my $git_status = run_command('/usr/bin/git status', {ignore_exit=>1});
+my $git_status = run_command( '/usr/bin/git status', { ignore_exit => 1 } );
 print "Git status:\n$git_status\n" if $conf{'verbose'};
 
-if ($git_status =~ /nothing to commit/) {
+if ( $git_status =~ /nothing to commit/ ) {
     print "Nothing to commit, exiting...\n";
 }
 else {
 
     # first schedule the changes
-    foreach my $line (split(/\n/, $git_status)) {
+    foreach my $line ( split( /\n/, $git_status ) ) {
+
         #print "processing $line\n" if $conf{'verbose'};
-        if ($line =~ /^#\tmodified: +(.*)$/) {
+        if ( $line =~ /^#\tmodified: +(.*)$/ ) {
             my $file = $1;
             print "Adding modified file: $file\n";
-            run_command("/usr/bin/git add $file", {modifies => 1});
+            run_command( "/usr/bin/git add $file", { modifies => 1 } );
         }
-        elsif ($line =~ /^#\tdeleted: +(.*)$/) {
+        elsif ( $line =~ /^#\tdeleted: +(.*)$/ ) {
             my $file = $1;
             print "Removing deleted file: $file\n";
-            run_command("/usr/bin/git rm $file", {modifies => 1});
+            run_command( "/usr/bin/git rm $file", { modifies => 1 } );
         }
-        elsif ($line =~ /^#\t(.*)$/) {
+        elsif ( $line =~ /^#\t(.*)$/ ) {
             my $file = $1;
             print "Adding new file: $file\n";
-            run_command("/usr/bin/git add $file", {modifies => 1});
+            run_command( "/usr/bin/git add $file", { modifies => 1 } );
         }
     }
 
     # now commit
     print "Committing with message '$conf{'commit-message'}'\n";
-    run_command("/usr/bin/git commit -m \"$conf{'commit-message'}\"", {modifies => 1});
+    run_command( "/usr/bin/git commit -m \"$conf{'commit-message'}\"",
+        { modifies => 1 } );
 
     # then push to the remote
     print "Pushing to backup remote: $conf{'remote'}\n";
-    run_command("/usr/bin/git push $conf{'remote'}", {modifies => 1});
+    run_command( "/usr/bin/git push $conf{'remote'}", { modifies => 1 } );
 }
 
 exit;
 
 sub print_configuration {
     print "Configuration after parsing options:\n";
-    printf " path: %s\n", $conf{'path'};
+    printf " path: %s\n",   $conf{'path'};
     printf " remote: %s\n", $conf{'remote'};
-    printf " database: %s\n", $conf{'database'} ? $conf{'database'} : '<none specified>';
-    printf " prefix: %s\n", $conf{'prefix'} ? $conf{'prefix'} : '<none specified>';
-    printf " mysql-defaults: %s\n", $conf{'mysql-defaults'} ? $conf{'mysql-defaults'} : '<none specified>';
+    printf " database: %s\n",
+        $conf{'database'} ? $conf{'database'} : '<none specified>';
+    printf " prefix: %s\n",
+        $conf{'prefix'} ? $conf{'prefix'} : '<none specified>';
+    printf " mysql-defaults: %s\n",
+        $conf{'mysql-defaults'}
+        ? $conf{'mysql-defaults'}
+        : '<none specified>';
     printf " verbose: %s\n", $conf{'verbose'} ? 'true' : 'false';
-    printf " test: %s\n", $conf{'test'} ? 'true' : 'false';
-    printf " commit-message: '%s'\n", $conf{'commit-message'} ? $conf{'commit-message'} : '<none specified>';
+    printf " test: %s\n",    $conf{'test'}    ? 'true' : 'false';
+    printf " commit-message: '%s'\n",
+        $conf{'commit-message'}
+        ? $conf{'commit-message'}
+        : '<none specified>';
 }
 
 sub run_command {
-	my ($cmd, $options) = @_;
-	print "Running command: $cmd\n" if $conf{'verbose'};
+    my ( $cmd, $options ) = @_;
+    print "Running command: $cmd\n" if $conf{'verbose'};
 
-    return if ($options->{modifies} && $conf{'test'});
+    return if ( $options->{modifies} && $conf{'test'} );
 
     my $output = `$cmd`;
-    my $rc = $? >> 8;
-    if (!$options->{'ignore_exit'} && $rc) {
+    my $rc     = $? >> 8;
+    if ( !$options->{'ignore_exit'} && $rc ) {
         die "$cmd failed with exit code $rc:\n$output\n";
     }
 
-	return $output;
+    return $output;
 }
-
 
 __END__
 
